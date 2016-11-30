729603fb42f7b1c867ccfc7f7ccecf101f845a68
Scott Munro <scottnmunro@gmail.com>
2016-11-29 22:47:52 -0600

    fix: added a simple nav mesh and a monkey muck spawner

---
+++ VirtualEnvironment/Assets/Scripts/MonkeyMuckSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class MonkeyMuckSpawner : MonoBehaviour
+{
+    [SerializeField] float SpawnDelay;
+    HashSet<GameObject> mFree;
+    HashSet<GameObject> mInUse;
+
+    public void Despawn(GameObject monkeyMuckToDespawn)
+    {
+        UnityEngine.Assertions.Assert.IsTrue(mInUse.Contains(monkeyMuckToDespawn));
+        monkeyMuckToDespawn.SetActive(false);
+        monkeyMuckToDespawn.transform.localPosition = Vector3.zero;
+        mInUse.Remove(monkeyMuckToDespawn);
+        mFree.Add(monkeyMuckToDespawn);
+    }
+
+    void Start()
+    {
+        mFree = new HashSet<GameObject>();
+        mInUse = new HashSet<GameObject>();
+
+        foreach (Transform child in transform)
+        {
+            mFree.Add(child.gameObject);
+        }
+        StartCoroutine(Spawner());
+    }
+
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            var playerNotClose = true;
+            if (mFree.Count > 0 && playerNotClose)
+            {
+                var nextMuck = popFree();
+                nextMuck.SetActive(true);
+                mInUse.Add(nextMuck);
+            }
+            yield return new WaitForSeconds(SpawnDelay);
+        }
+    }
+
+    GameObject popFree()
+    {
+        var itr = mFree.GetEnumerator();
+        itr.MoveNext();
+        var go = itr.Current;
+        mFree.Remove(go);
+        return go;
+    }
+}
---
+++ VirtualEnvironment/Assets/Scripts/WalkToDestination.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkToDestination : MonoBehaviour
+{
+    [SerializeField] GameObject Destination;
+    NavMeshAgent InternalNavMeshAgent;
+    MonkeyMuckSpawner Spawner;
+
+    // Use this for initialization
+    void Start()
+    {
+        InternalNavMeshAgent = GetComponent<NavMeshAgent>();
+        Spawner = transform.parent.gameObject.GetComponent<MonkeyMuckSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////
+        //You don't have to include this one line code:
+        //I included this line to correct my enemy's rotation, you might not need this one line.
+        //Or you may have to change the rotation to fit your character.
+        //gameObject.transform.Rotate(270, 0, 0);
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        InternalNavMeshAgent.SetDestination(Destination.transform.position);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == Destination)
+        {
+            Spawner.Despawn(gameObject);
+        }
+    }
+}
