1539232a5a717cbbfe8f7785e7fd37712d855972
Scott Munro <scottnmunro@gmail.com>
2016-11-30 01:20:09 -0600

    fix: orbit camera playing a little bit nicer

---
+++ VirtualEnvironment/Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCollision : MonoBehaviour
+{
+    [SerializeField]
+    float setDistance;
+    [SerializeField]
+    LayerMask cameraCollision;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0,0,0);
+    }
+    void Update()
+    {
+        Vector3 dir = (transform.position - transform.parent.position);
+        Debug.DrawRay(transform.parent.position, dir, Color.red);
+        Debug.DrawLine(Vector3.zero, transform.parent.position, Color.blue);
+        Debug.DrawLine(Vector3.zero, transform.position, Color.green);
+        var raycastHits = Physics.RaycastAll(transform.parent.position, dir, setDistance, cameraCollision);
+        if (raycastHits.Length != 0)
+        //if (Physics.Linecast(transform.parent.position, transform.parent.position + dir, out hit))
+        //if (Physics.Linecast(transform.parent.position, transform.position, out hit))
+        {
+            float minDistance = Mathf.Infinity;
+            foreach (RaycastHit h in raycastHits)
+            {
+                minDistance = Mathf.Min(minDistance, h.distance);
+            }
+            offset.z = -minDistance + .2f;
+        }
+        else
+        {
+            offset.z = -setDistance;
+        }
+
+        transform.localPosition = offset;
+    }
+}
--- VirtualEnvironment/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ VirtualEnvironment/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -46,7 +46,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            //float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
             bool crouch = Input.GetKey(KeyCode.C);
 
@@ -56,12 +56,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+                m_Move = v * m_CamForward; //+ h*m_Cam.right;
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
+                m_Move = v * Vector3.forward; //+ h*Vector3.right;
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
