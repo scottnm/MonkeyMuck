43373dffce24d34f7668160713b5c9af951c1d0f
Scott Munro <scottnmunro@gmail.com>
2016-12-01 00:35:55 -0600

    feat: added zero'd out velocity warp zone

---
+++ VirtualEnvironment/Assets/Warphole.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Filibusters
+{
+    public class Warphole : MonoBehaviour
+    {
+        [SerializeField] int warpCount;
+        Dictionary<GameObject, int> WarpCounts;
+
+        void Start()
+        {
+            WarpCounts = new Dictionary<GameObject, int>();
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            var go = col.gameObject;
+            int count;
+
+            if (WarpCounts.ContainsKey(go))
+            {
+                count = WarpCounts[go];
+            }
+            else
+            {
+                WarpCounts[go] = 0;
+                count = 0;
+            }
+
+            WarpCounts[go] = ++count;
+
+            if (count <= warpCount)
+            {
+                var goPos = go.transform.position;
+                goPos.y = transform.position.y;
+                go.transform.position = goPos;
+            }
+            else
+            {
+                WarpCounts[go] = 0;
+            }
+        }
+    }
+}
---
+++ VirtualEnvironment/Assets/ZeroVelocity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class ZeroVelocity : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider col)
+        {
+            var vel = col.gameObject.GetComponent<Rigidbody>().velocity;
+            vel.x = 0;
+            vel.z = 0;
+            col.gameObject.GetComponent<Rigidbody>().velocity = vel;
+        }
+    }
+}
