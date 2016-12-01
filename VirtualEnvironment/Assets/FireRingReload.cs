e0fe7afe1260644a6867d26e2698237830ec628f
Scott Munro <scottnmunro@gmail.com>
2016-12-01 03:01:22 -0600

    feat: got the fade and restart going

--- VirtualEnvironment/Assets/FireRingReload.cs
+++ VirtualEnvironment/Assets/FireRingReload.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class FireRingReload : MonoBehaviour
 {
+    [SerializeField]
+    float fadeTime;
+    [SerializeField]
+    float endFadeIntensity;
+
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            var controlScript = col.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>();
+            if (controlScript.enabled)
+            {
+                controlScript.enabled = false;
+                StartCoroutine(RunFadeAndReload());
+            }
         }
+    }
+
+    IEnumerator RunFadeAndReload()
+    {
+        var overlayScript = GameObject.FindObjectOfType<Camera>().gameObject.GetComponent<UnityStandardAssets.ImageEffects.ScreenOverlay>();
+        float time = 0;
+        while (time < fadeTime)
+        {
+            overlayScript.intensity = Mathf.Lerp(0, endFadeIntensity, time / fadeTime);
+            yield return new WaitForFixedUpdate();
+            time += Time.deltaTime;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
