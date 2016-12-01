84daee31800c95e3065914a46cad4469cbcd9e11
Scott Munro <scottnmunro@gmail.com>
2016-12-01 02:19:31 -0600

    fix: added firering. still a little bit buggy here and there

---
+++ VirtualEnvironment/Assets/RotateMonkeyMuckRing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class RotateMonkeyMuckRing : MonoBehaviour
+    {
+        [SerializeField]
+        float rotateSpeed;
+
+        void Update()
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
--- VirtualEnvironment/Assets/ZeroVelocity.cs
+++ VirtualEnvironment/Assets/ZeroVelocity.cs
@@ -1,16 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace Filibusters
+public class ZeroVelocity : MonoBehaviour
 {
-    public class ZeroVelocity : MonoBehaviour
+    void OnTriggerEnter(Collider col)
     {
-        void OnTriggerEnter(Collider col)
+        var vel = col.gameObject.GetComponent<Rigidbody>().velocity;
+        vel.x = 0;
+        vel.z = 0;
+        col.gameObject.GetComponent<Rigidbody>().velocity = vel;
+
+        StartCoroutine(LerpToCenterOfTunnel(col.gameObject));
+    }
+    IEnumerator LerpToCenterOfTunnel(GameObject go)
+    {
+        float time = 0f;
+        Vector3 posBuffer = new Vector3();
+
+        float original_x = go.transform.position.x;
+        float original_z = go.transform.position.z;
+
+        while (time <= 1)
         {
-            var vel = col.gameObject.GetComponent<Rigidbody>().velocity;
-            vel.x = 0;
-            vel.z = 0;
-            col.gameObject.GetComponent<Rigidbody>().velocity = vel;
+            posBuffer.x = Mathf.Lerp(original_x, transform.position.x, time);
+            posBuffer.y = go.transform.position.y;
+            posBuffer.z = Mathf.Lerp(original_z, transform.position.z, time);
+
+            go.transform.position = posBuffer;
+
+            yield return new WaitForFixedUpdate();
+            time += Time.deltaTime;
         }
+        var vel = go.GetComponent<Rigidbody>().velocity;
+        vel.x = 0;
+        vel.z = 0;
+        go.GetComponent<Rigidbody>().velocity = vel;
     }
 }
