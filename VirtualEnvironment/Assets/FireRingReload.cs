9dab2b2a55dd4c2b56bbb251a13554e6f69961dc
Scott Munro <scottnmunro@gmail.com>
2016-12-01 02:40:28 -0600

    feat: reset scene on fire contact

---
+++ VirtualEnvironment/Assets/FireRingReload.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FireRingReload : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
