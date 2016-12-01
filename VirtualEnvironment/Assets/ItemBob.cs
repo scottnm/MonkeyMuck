53b4aeca22285b770b37160481c9b47307b6b655
Scott Munro <scottnmunro@gmail.com>
2016-12-01 02:26:11 -0600

    added in keybob, moved player back, readded door

---
+++ VirtualEnvironment/Assets/ItemBob.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBob : MonoBehaviour
+{
+    [SerializeField]
+    AnimationCurve mAnimationCurve;
+
+    float mTime;
+    Vector3 mOrigin;
+    Vector3 mYDisplacement;
+
+    void Start()
+    {
+        mTime = 0f;
+        mOrigin = transform.position;
+        mYDisplacement = new Vector3(0, 0.5f, 0);
+    }
+
+    void Update()
+    {
+        var displacementRatio = mAnimationCurve.Evaluate(mTime);
+        transform.position = mYDisplacement * displacementRatio + mOrigin;
+        mTime += Time.deltaTime;
+    }
+}
